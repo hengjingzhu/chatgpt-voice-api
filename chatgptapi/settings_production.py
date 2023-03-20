c495efa5981fe37f11e7b91ee48086e8449991f0
davidzhu <david3311041@hotmail.com>
2023-03-20 16:47:04 +0800

    第一次提交

--- chatgptapi/settings_production.py
+++ chatgptapi/settings_production.py
@@ -200,7 +200,7 @@ SIMPLEUI_DEFAULT_THEME = 'e-black-pro.css'
 SIMPLEUI_LOGO = '/static/image/adai.png'
 
 # character reset commamd
-RESET_CHARACTER_COMMAMD = ['重置角色','角色重置','reset the character','reset character','reset character']
+RESET_CHARACTER_COMMAMD = ['重置角色','角色重置','reset the character','reset character','reset your character','character reset']
 
 
 
